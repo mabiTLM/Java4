cbf809786f68028b055c355ab7de79702fa63f04
mabiTLM <mabitlm@naver.com>
2023-08-23 10:07:06 +0900

    생성 : persona 브랜치 , 수정 : 주석과 가독성 수정

--- HomeworkPersona/src/HomeworkPersona/Persona5_0821.java
+++ HomeworkPersona/src/HomeworkPersona/Persona5_0821.java
@@ -1,7 +1,8 @@
 package HomeworkPersona;
 
 
-class Player //플레이어 캐릭터
+
+class Player//플레이어 캐릭터
 {
 	String name;
 	int level = 1;
@@ -11,20 +12,19 @@ class Player //플레이어 캐릭터
 	int gentleness=1;//상냥함
 	int guts=1;//배짱
 	int charm=1;//매력
-	
 	String currentPersona = "아르센";
-	String PossessedPersona[]; //소지 페르소나
+	String PossessedPersona[];//소지 페르소나
 	String itemBox; //아이템 클래스를 따로만들고 불러오는 방식
 }
 
 
 
-class TotalPersonaBook //페르소나 전서
+class TotalPersonaBook//페르소나 전서
 {
 	String personaName[] ={"아르센","픽시"} ;
 	String personaArcana[] ={"광대","연인"} ;
 	int personaDefaultLevel[] = {1,2};
-	boolean confirmedpersona[] = {true,false}; //페르소나 확인여부
+	boolean confirmedpersona[] = {true,false};//페르소나 확인여부
 	
 	void personaBookList()
 	{
@@ -34,11 +34,12 @@ class TotalPersonaBook //페르소나 전서
 			System.out.println(personaName[i] + "	" + personaArcana[i] + "	" + personaDefaultLevel[i]);
 		}
 	}
-
+	
 }
 
 
-class Arsene //공통부분으로 상위클래스를 만들어서 오버라이드 할 수 있을 것 같습니다.
+
+class Arsene//공통부분으로 상위클래스를 만들어서 오버라이드 할 수 있을 것 같습니다.
 {
 	PersonaSkillInfo PersonaSkillInfo = new PersonaSkillInfo();
 	final String name = "아르센";
@@ -62,16 +63,13 @@ class Arsene //공통부분으로 상위클래스를 만들어서 오버라이�
 		luck = luck+1;
 	}
 	
-	static final String arcana = "광대"; //아르카나는 변화하지 않는다.
-	String[] weak = {"얼음", "빛"}; //공격받을 때 공격속성이 일치하는게 있으면
+	static final String arcana = "광대";//아르카나는 변화하지 않는다.
+	String[] weak = {"얼음", "빛"};//공격받을 때 공격속성이 일치하는게 있으면
 	String[] resist = {"어둠"}; ////공격할 때 공격속성이 일치하는게 있으면
 	String changeItem[] = {"괴도신사 스틱","대 괴도 스틱"};//통상인지 경보인지는 아이템화 시키는 곳에서
-	static final String summonSpeech = "내 이름은 황혼의 약탈자 아르센! 다시금 나의 힘이 필요한가...? 좋다. 마음껏 휘둘러 보거라.";
-	
-	//생성할 때 호출
-	String[] specificity = {"배수의 닻","배턴 터치를 받았을 때, 포위당했을 때의 전용 스킬을 사용할 수 있다","계승 가능"};
-	//효과를 불러올때는 0번으로 계승여부확인은 2번으로
-	String[] nowSkill = {"에이하","","","","","","",""}; //페르소나가 소지할 수 있는 스킬 8개
+	static final String summonSpeech = "내 이름은 황혼의 약탈자 아르센! 다시금 나의 힘이 필요한가...? 좋다. 마음껏 휘둘러 보거라.";//생성할 때 호출
+	String[] specificity = {"배수의 닻","배턴 터치를 받았을 때, 포위당했을 때의 전용 스킬을 사용할 수 있다","계승 가능"};//효과를 불러올때는 0번으로 계승여부확인은 2번으로
+	String[] nowSkill = {"에이하","","","","","","",""};//페르소나가 소지할 수 있는 스킬 8개
 	String canLearnSkillname[] = {"에이하","슬래시","스쿤다","몽견침","역경의 각오"};
 	int canLearnSkillLevel[] = {1,2,4,5,7};
 	String[] learnSkillInfo = {//이름과 뒷부분 분리후 배울 수 있는 스킬과 대조해서 불러오자
@@ -81,7 +79,8 @@ class Arsene //공통부분으로 상위클래스를 만들어서 오버라이�
 			"스쿤다"," 4 SP 8 3턴간 적 하나의 명중율, 회피율 저하\n",
 			"몽견침"," 5 HP 8.0% 적 하나에게 물리 속성 중 대미지. 중간 확률로 수면\n",
 			"역경의 각오"," 7 - 포위되었을 때 크리티컬 확률이 상승\n"};
-	void printLearnSkill() //합치는 부분에 넣을 수 있을 것 같은 부분
+	
+	void printLearnSkill()//합치는 부분에 넣을 수 있을 것 같은 부분
 	{
 		for(int i = 0; i < learnSkillInfo.length; i++)
 		{
@@ -89,18 +88,19 @@ class Arsene //공통부분으로 상위클래스를 만들어서 오버라이�
 		}
 		
 	}
+	
 	void learn()//스킬 이름을 가져온후 습득레벨과 비교한다 , 레벨업 했을 때 한번 수행
 	{
 		int temp = 1;
 		for(int i = 0; i <PersonaSkillInfo.skillname.length; i++)// 전체 스킬 목록에서
 		{
-			for(int j = 0; j <canLearnSkillname.length; j++) //배울 수 있는 스킬이 있는가?
+			for(int j = 0; j <canLearnSkillname.length; j++)//배울 수 있는 스킬이 있는가?
 				{
 				if(PersonaSkillInfo.skillname[i].equals(canLearnSkillname[j]))
 				{
 					if(this.level==canLearnSkillLevel[j])//레벨이 같으면
 						{
-						if(nowSkill[temp].isEmpty()) //빈공간이있으면 추가
+						if(nowSkill[temp].isEmpty())//빈공간이있으면 추가
 							{
 							nowSkill[temp] = PersonaSkillInfo.skillname[i];
 							break;
@@ -123,7 +123,10 @@ class Arsene //공통부분으로 상위클래스를 만들어서 오버라이�
 			}
 		}
 	}
+	
 }
+
+
 
 class Pixie{
 	PersonaSkillInfo PersonaSkillInfo = new PersonaSkillInfo();
@@ -138,6 +141,7 @@ class Pixie{
 	int speed = 4;
 	int luck = 2;
 	int exp = 0;
+	
 	void levelup()//레벨당 자료가 없어서 임의 수치
 	{
 		level = level+1;
@@ -148,14 +152,13 @@ class Pixie{
 		luck = luck+2;
 	}
 	
-	static final String arcana = "연인"; //아르카나는 변화하지 않는다.
-	String[] weak = {"총격", "얼음","어둠"}; //공격받을 때 공격속성이 일치하는게 있으면
+	static final String arcana = "연인";//아르카나는 변화하지 않는다.
+	String[] weak = {"총격", "얼음","어둠"};//공격받을 때 공격속성이 일치하는게 있으면
 	String[] resist = {"전기","빛"}; ////공격할 때 공격속성이 일치하는게 있으면
 	String changeItem[] = {"지오(스킬 카드)","마하지오(스킬 카드)"};//통상인지 경보인지는 아이템화 시키는 곳에서
 	static final String summonSpeech = "난 픽시야! 네 가면이 되어서 같이 소란피울 거야!";
-	String[] specificity = {"추가 전도체","다운된 적에 대한 감전율이 상승한다","계승 가능"};
-	//효과를 불러올때는 0번으로 계승여부확인은 2번으로
-	String[] nowSkill = {"지오","디아","","","","","",""}; //페르소나가 소지할 수 있는 스킬 8개
+	String[] specificity = {"추가 전도체","다운된 적에 대한 감전율이 상승한다","계승 가능"};//효과를 불러올때는 0번으로 계승여부확인은 2번으로
+	String[] nowSkill = {"지오","디아","","","","","",""};//페르소나가 소지할 수 있는 스킬 8개
 	String canLearnSkillname[] = {"지오","디아","파트라","타루카쟈","혼란 내성"};
 	String[] learnSkill = {
 			"이름, 습득레벨, 소비, 설명",
@@ -165,18 +168,19 @@ class Pixie{
 			"타루카쟈 5 SP 8 적 하나에게 물리 속성 중 대미지. 중간 확률로 수면,",
 			"혼란 내성 6 - 포위되었을 때 크리티컬 확률이 상승"};
 	int[] canLearnSkillLevel = {1,1,3,5,6};
+	
 	void learn()//스킬 이름을 가져온후 습득레벨과 비교한다 , 레벨업 했을 때 한번 수행
 	{
 		int temp = 1;
-		for(int i = 0; i <PersonaSkillInfo.skillname.length; i++)// 전체 스킬 목록에서
+		for(int i = 0; i <PersonaSkillInfo.skillname.length; i++)//전체 스킬 목록에서
 		{
-			for(int j = 0; j <canLearnSkillname.length; j++) //배울 수 있는 스킬이 있는가?
+			for(int j = 0; j <canLearnSkillname.length; j++)//배울 수 있는 스킬이 있는가?
 			{
 				if(PersonaSkillInfo.skillname[i].equals(canLearnSkillname[j]))
 				{
 					if(this.level==canLearnSkillLevel[i])//레벨이 같으면
 					{
-						if(nowSkill[temp].isEmpty()) //빈공간이있으면 추가
+						if(nowSkill[temp].isEmpty())//빈공간이있으면 추가
 						{
 							nowSkill[temp] = PersonaSkillInfo.skillname[i];
 							break;
@@ -205,7 +209,7 @@ class Pixie{
 
 class PersonaSkillInfo
 {
-	enum Persona {에이하,슬래시,스쿤다,몽견침,역경의_각오,지오,디아,파트라,타루카쟈,혼란_내성}; // 스킬이 몇번이지 까먹지 않기위해서
+	enum Persona {에이하,슬래시,스쿤다,몽견침,역경의_각오,지오,디아,파트라,타루카쟈,혼란_내성};//스킬이 몇번이지 까먹지 않기위해서
 	String skillname[] = {"에이하","슬래시","스쿤다","몽견침","역경의 각오","지오","디아","파트라","타루카쟈","혼란 내성"};
 	int useMp[] = {4,0,8,0,0,4,3,4,8,0};
 	int useHp[] = {0,6,0,8,0,0,0,0,0,0};
@@ -216,14 +220,10 @@ class PersonaSkillInfo
 
 
 
-
-
 class MixPersona //합성
 {
 	TotalPersonaBook totalPersonaBook = new TotalPersonaBook();//합성시 결과물의 조건을 위해 필요
 	Player player = new Player();//합성시 페르소나 갱신을 위해 필요
-	
-	
 	String[] personnaArcana = {"광대","마법사","여법황","여황제","황제","법황","연인","전차","정의","은둔자","운명","힘","사형수","사신","절제","악마","탑","별","달","태양","심판","세계"};
 	String[] fusionResultTable = 
 		{
@@ -249,7 +249,6 @@ class MixPersona //합성
 			"태양","사신",
 			"심판"
 		};
-			
 	String[] fusion = new String[(personnaArcana.length-1)*personnaArcana.length/2+personnaArcana.length];
 	String resultArcana;
 	int resultLevel;
@@ -275,7 +274,6 @@ class MixPersona //합성
 		}
 	}
 	
-	
 	void getfusionTable()
 	{
 		int temp = 22;
@@ -294,47 +292,38 @@ class MixPersona //합성
 	
 	void mix()
 	{
-		resultArcana = a.arcana +p.arcana; //선택한 페르소나의 아르카나 합성
-		String resultArcanaReverse = p.arcana +a.arcana; //역순도 찾기
-		for(int i = 0; i <fusion.length; i++)//조건이 뭘까? 수동? 규칙?
+		resultArcana = a.arcana +p.arcana;//선택한 페르소나의 아르카나 합성
+		String resultArcanaReverse = p.arcana +a.arcana;//역순도 찾기
+		for(int i = 0; i <fusion.length; i++)//조건이 뭘까? 수동? 규칙? 일단 표자체를 코딩
 			{
-				if(fusion[i].equals(resultArcana))
+				if(fusion[i].equals(resultArcana)|fusion[i].equals(resultArcanaReverse))
 				{
 					mixPersonaResult=resultArcana;
 					break;
 				}
-				else if(fusion[i].equals(resultArcanaReverse))
-				{
-					mixPersonaResult=resultArcana;
-					break;
-				}
-				
 				else if(i==fusion.length-1)//합성불가능
 				{
 					mixPersonaResult="합성 룰가능";
 					break;
 				}
 			}
-		
-		
-		
-		resultLevel=a.level/p.level+1;
-		
-		
-		
-		//소지페르소나 = 전체페르소나(mixPersonaResult아르카나,resultLevel레벨) 모양으로 일치하는걸 찾아야한다
+		resultLevel=a.level/p.level+1;//소지페르소나 = 전체페르소나(mixPersonaResult아르카나,resultLevel레벨) 모양으로 일치하는걸 찾아야한다
 		}
 	}
+
+
+
 public class Persona5_0821
 {
 	public static void main(String[] args)
 	{
+		//실행 확인용 코드들
 		// Arsene a = new Arsene();
 		// a.printLearnSkill();
 		
-		MixPersona m = new MixPersona();
-		m.fusionTable();
-		m.getfusionTable();
+		//MixPersona m = new MixPersona();
+		//m.fusionTable();
+		//m.getfusionTable();
 	}
 
 }
