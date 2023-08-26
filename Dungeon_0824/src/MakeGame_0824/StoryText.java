afb5064cc6f49cf0aa91bfaa982f38b10047ed0f
mabiTLM <mabitlm@naver.com>
2023-08-27 00:20:54 +0900

    합친 후 수정

--- Dungeon_0824/src/MakeGame_0824/StoryText.java
+++ Dungeon_0824/src/MakeGame_0824/StoryText.java
@@ -14,17 +14,16 @@ int evilCount = 0;
 int goodCount = 0;
 
 
-private String start ="이동과 선택지는 숫자로 선택하며 스킬발동은 가진 스킬을 입력해서 사용합니다."
+private String start ="이동과 선택지는 숫자로 선택하며 스킬발동은 가진 스킬을 입력해서 사용합니다.";
 
-String[] mainStory={}
+String[] mainStory={};
 
 String[] endingStory=
 {
-"엔딩0 다시 일상으로/n동생은 완치 됐습니다.쌩쌩한것 앞으로 100년은 더 살것처럼보이네요."
+"엔딩0 다시 일상으로/n동생은 완치 됐습니다.쌩쌩한것 앞으로 100년은 더 살것처럼보이네요.",
 "엔딩1 헛된 죽음/n당신은 던전에서 죽었습니다./n혼자 남겨진 당신의 동생도 곧 뒤따라가겠지요.",
 "엔딩2 ",
-
-}
+};
 
 
 void timeRemaining()
@@ -62,6 +61,7 @@ return mainStory;
 }
 public String[] getEndingStory()
 {
-return endingStory
+return endingStory;
 }
+
 }
