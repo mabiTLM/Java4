14ff5923a395a1c127c721c06d38c4873f2199a9
mabiTLM <90094200+mabiTLM@users.noreply.github.com>
2023-08-26 10:49:43 +0900

    스토리 추가중

--- Dungeon_0824/src/MakeGame_0824/StoryText.java
+++ Dungeon_0824/src/MakeGame_0824/StoryText.java
@@ -1,6 +1,20 @@
 package MakeGame_0824;
 
-public class StoryText //모바일 실행테스트
+public class StoryText
 {
-	
+private String start ="이동과 선택지는 숫자로 선택하며 스킬발동은 가진 스킬을 입력해서 사용합니다"
+
+String[] mainStory={}
+
+String[] endingStory=
+{
+"엔딩1 ",
+"",
+
+}
+
+public String getStart()
+{
+return start;
+}
 }
