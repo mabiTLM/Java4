961951c18625edca6e5707a0761ffcf5241a6833
mabiTLM <mabitlm@naver.com>
2023-09-08 21:00:32 +0900

    stream실험

--- webClass230908/src/webClass230908/ModuleTest.java
+++ webClass230908/src/webClass230908/ModuleTest.java
@@ -8,15 +8,25 @@ public class ModuleTest {
 	
 	public static void main(String[] args)
 	{
-		StreamModule<ArrayList> test = new StreamModule();
-		List<String> arr = new ArrayList();
+		Stream streamTest;
+		StreamModule<List> test = new StreamModule();
+		List<ArrayList> arr = new ArrayList();
+		List<String> str = new ArrayList();
+		str.add("이건되나?");
+		str.add("형태가 어떤거지");
+		arr.add((ArrayList) str);
 		
-		arr.add("확인용");
-		
-		for(String output : arr)
+		for(ArrayList output : arr)
 		{
-			System.out.println(test.changeStream(output));
+			System.out.println(output);
 		}
+		streamTest = test.changeStream(arr);//스트림으로 바꿔주는 거 완료
+		
+		test.changeStream(arr).forEach(item->System.out.println(item));//스트림을 따로 정의 안해줘도되는 이 모양이 그나마 가장 나은듯하다.
+		streamTest.forEach(item->System.out.println(item));
+		//streamTest.forEach(item->System.out.println(item)); 이것도 결국 2연속은 안되니까 매번 다시 만들어줘야한다.
+		
+		test.print(arr);//그냥 이렇게 쓰면될것같다.
 	}
 
 }
--- webClass230908/src/webClass230908/StreamModule.java
+++ webClass230908/src/webClass230908/StreamModule.java
@@ -5,10 +5,27 @@ import java.util.stream.Stream;
 
 public class StreamModule <T extends Collection>{
 	
-	public Stream<T> changeStream(T t)
+	private T t;
+	
+	public Stream<T> changeStream(T t)//넣은걸 스트림으로 바꿔서 리턴한다.
 	{
+		this.t=t;
 		Stream<T> result = t.stream();
 		return result;
 	}
+	
+	public void print(T t)//넣은걸 스트림으로 바꿔서 출력한다.
+	{
+		this.t=t;
+		Stream<T> result = t.stream();
+		result.forEach(item->System.out.println(item));
+	}
+	
+	
+//	@Override
+//	public String toString() //제네릭이면 내부에서 바뀌긴힘들것같다.
+//	{
+//		return t+"";
+//	}
 
 }
