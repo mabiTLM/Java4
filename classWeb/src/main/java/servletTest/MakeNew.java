9d4314891ca90337ecb7ca7ccef56599a021f359
mabiTLM <mabitlm@naver.com>
2023-09-19 14:11:58 +0900

    브랜치 변경전 저장용

---
+++ classWeb/src/main/java/servletTest/MakeNew.java
@@ -0,0 +1,47 @@
+package servletTest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MakeNew
+ */
+@WebServlet("/MakeNew")
+public class MakeNew extends HttpServlet {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 6370713015178192768L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public MakeNew() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    response.getWriter().append("Served at: ").append(request.getContextPath());
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+
+}
