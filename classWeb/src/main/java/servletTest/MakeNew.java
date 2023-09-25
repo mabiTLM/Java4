bb29fefe8c2f81c1741b11329f86b779ec9ef08e
mabiTLM <mabitlm@naver.com>
2023-09-25 09:52:40 +0900

    변경사항

--- classWeb/src/main/java/servletTest/MakeNew.java
+++ classWeb/src/main/java/servletTest/MakeNew.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import makeCardGame230901.cardBundle.TotalCardBase;
 
-
 @WebServlet("/MakeNew")
 public class MakeNew extends HttpServlet {
   /**
@@ -23,21 +22,24 @@ public class MakeNew extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     response.setCharacterEncoding("UTF-8");
-    response.getWriter().append("<html>").append("<head>")
-        .append("<meta charset='UTF-8'/>" + "<title>게임</title>");
+    response.getWriter().append("<html>" + "<head>" + "<meta charset='UTF-8'/>"
+        + "<title>게임</title>" + "</head>" + "<body>" + "<form action=\"\">");
+
+
+    response.getWriter().append("<input type=\"text\">" + "<input type=\"submit\">");
+
+
     TotalCardBase card = new TotalCardBase();
     response.getWriter().append(card.watchCardData(card.totalCard()).replace("\n", "<br>"));
-    response.getWriter().append("<input type=\"text\">");
-    response.getWriter().append("</head><html>");
-  }
 
-  public void a() {
-    System.out.println("실험");
+    response.getWriter().append("</form></body></html>");
   }
+  
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     doGet(request, response);
+
   }
 
 }
