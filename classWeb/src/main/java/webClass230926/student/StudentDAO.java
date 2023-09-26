6e9e189d44f152292c1f465e21c604b0aa69b324
mabiTLM <mabitlm@naver.com>
2023-09-26 12:02:17 +0900

    임포트 이상하게 받아서 다시 받기위한 준비

--- classWeb/src/main/java/webClass230926/student/StudentDAO.java
+++ classWeb/src/main/java/webClass230926/student/StudentDAO.java
@@ -1,12 +1,13 @@
 package webClass230926.student;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class StudentDAO {
   private Connection con;
@@ -20,8 +21,10 @@ public class StudentDAO {
       // Connection con =
       // DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "java", "qwer");
       String query = "select * from student";
-      Statement stmt = con.createStatement();
-      ResultSet rs = stmt.executeQuery(query);
+      PreparedStatement stmt = con.prepareStatement(query);
+      ResultSet rs = stmt.executeQuery();
+      // Statement stmt = con.createStatement();
+      // ResultSet rs = stmt.executeQuery(query);
       while (rs.next()) {
         StudentVO temp =
             new StudentVO(rs.getInt("id"), rs.getString("name"), rs.getString("student_id"),
@@ -38,11 +41,15 @@ public class StudentDAO {
   }
 
   private void connect() throws Exception {
-    String query = "select * from student";
-    Class.forName("oracle.jdbc.OracleDriver");
-    con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "java", "qwer");
-    PreparedStatement stmt = con.prepareStatement(query);
-    ResultSet rs = stmt.executeQuery();
-  }
 
+    // Class.forName("oracle.jdbc.OracleDriver");
+    // con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "java", "qwer");
+
+    Context ctx = new InitialContext();
+    // DriverManager =>DM
+    // initialize 초기화
+    Context envContext = (Context) ctx.lookup("java:/comp/env");
+    DataSource dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+    con = dataFactory.getConnection();
+  }
 }
