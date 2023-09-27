a2c235e204f15f3a58cbf74903079540059cf244
mabiTLM <mabitlm@naver.com>
2023-09-27 15:50:34 +0900

    동기화중 삭제된 스크립트 복구

--- classWeb/src/main/java/servletTest/Village.java
+++ classWeb/src/main/java/servletTest/Village.java
@@ -6,7 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import makeCardGame230901.cardBundle.TotalCardBase;
 
 @WebServlet("/village")
 public class Village extends HttpServlet {
@@ -22,26 +21,28 @@ public class Village extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     response.setCharacterEncoding("UTF-8");
-    response.getWriter().append("<!DOCTYPE html>"
-        + "<html lang=\"ko\">"
-        + "  <head>"
-        + "    <meta charset=\"UTF-8\" />"
-        + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />"
-        + "    <title>Document</title>"
-        + "    <script src=\"/scripts/makeCardGame.js\"></script>"
-        + "    <link rel=\"stylesheet\" href=\"/styles/style.css\" />"
-        + "  </head>"
-        + "  <body>"
-        + "    <button id=\"editDeck\" onclick=\"watchCard()\">덱수정</button>"
-        + "    <button id=\"editDeck\" onclick=\"add(1)\">카드추가</button>"
-        + "    <button>상점</button>"
-        + "    <a href=\"dungeon\"><button>모험으로</button></a>"
-        + "  </body>"
-        + "</html>"
-        + "");
+    String html = "";
+    html += "<!DOCTYPE html>";
+    html += "<html lang='ko'>";
+    html += "  <head>";
+    html += "    <meta charset='UTF-8' />";
+    html += "    <meta name='viewport' content='width=device-width, initial-scale=1.0' />";
+    html += "    <title>Document</title>";
+    html += "    <script src='/scripts/makeCardGame.js'></script>";
+    // html += " <link rel='stylesheet' href='/styles/style.css' />";
+    html += "  </head>";
+    html += "  <body>";
+    html += "    <button id='editDeck' onclick='watchCard()'>덱수정</button>";
+    html += "    <button id='editDeck' onclick='add(1)'>카드추가</button>";
+    html += "    <button>상점</button>";
+    html += "    <a href='dungeon'><button>모험으로</button></a>";
+    html += "  </body>";
+    html += "</html>";
+
+    response.getWriter().append(html);
   }
-//TotalCardBase card = new TotalCardBase();
-//response.getWriter().append(card.watchCardData(card.totalCard()).replace("\n", "<br>"));
+  // TotalCardBase card = new TotalCardBase();
+  // response.getWriter().append(card.watchCardData(card.totalCard()).replace("\n", "<br>"));
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
---
+++ classWeb/src/main/java/servletTest/scripts/script.js
@@ -0,0 +1,44 @@
+let card = ["1", "21", "3"];
+
+function setCookie(name, value) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + 30);
+  var cookie_value = escape(value) + "; expires=" + exdate.toUTCString();
+  document.cookie = name + "=" + cookie_value;
+}
+
+function getCookie(name) {
+  var x, y;
+  var val = document.cookie.split(";");
+  for (var i = 0; i < val.length; i++) {
+    x = val[i].substr(0, val[i].indexOf("="));
+    y = val[i].substr(val[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x == name) {
+      return unescape(y);
+    }
+  }
+}
+
+function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
+}
+
+function newGame() {
+  deleteCookie("deck");
+  alert("새 게임을 불러옵니다");
+  setCookie("deck", card);
+}
+
+function add(num) {
+  card = getCookie("deck").split(",");
+  card[card.length] = num;
+  setCookie("deck", card);
+}
+
+function watchCard() {
+  card = getCookie("deck").split(",");
+  for (let i = 0; i < card.length; i++) {
+    console.log(card[i]);
+  }
+}
