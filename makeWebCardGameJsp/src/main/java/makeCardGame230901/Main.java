28a0b1301d1b3005e9943ab575f8255c5bab5c45
mabiTLM <mabitlm@naver.com>
2023-10-12 23:13:47 +0900

    지우고 다시 복사

--- makeWebCardGameJsp/src/main/java/makeCardGame230901/Main.java
+++
@@ -1,9 +0,0 @@
-package makeCardGame230901;
-
-public class Main {
-
-  public static void main(String[] args) {
-    Title title = new Title();
-    title.title();
-  }
-}
--- makeWebCardGameJsp/src/main/java/makeCardGame230901/characterBundle/MONSTEREFFECT.java
+++
@@ -1,5 +0,0 @@
-package makeCardGame230901.characterBundle;
-
-public enum MONSTEREFFECT {
-  SHIELD, ATTACKUP, USESKILL
-}
