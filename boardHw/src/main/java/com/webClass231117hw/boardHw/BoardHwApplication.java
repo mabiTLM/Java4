53f4761a689c36838665c6f696f93f6c3a2556a2
mabiTLM <mabitlm@naver.com>
2023-11-17 15:12:36 +0900

    수업 내용 및 도메인 쿼리문 작성

---
+++ boardHw/src/main/java/com/webClass231117hw/boardHw/BoardHwApplication.java
@@ -0,0 +1,13 @@
+package com.webClass231117hw.boardHw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardHwApplication.class, args);
+	}
+
+}
---
+++ boardHw/src/test/java/com/webClass231117hw/boardHw/BoardHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webClass231117hw.boardHw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
