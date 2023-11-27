7d60cb581eb5cb186ff79dc9a9a0df027b17d4be
mabiTLM <mabitlm@naver.com>
2023-11-27 16:42:07 +0900

    시간나오고 4번째까지 불러오고 일반적으로 적용되는 함수 구현중

--- boardHw/src/main/resources/static/scripts/board/comment.js
+++ boardHw/src/main/resources/static/scripts/board/comment.js
@@ -1,3 +1,18 @@
+function dateChange(e) {
+  var tempDate = new Date(e);
+  return (
+    tempDate.getFullYear() +
+    "." +
+    tempDate.getMonth() +
+    "." +
+    tempDate.getDate() +
+    " " +
+    tempDate.getHours() +
+    " : " +
+    tempDate.getMinutes()
+  );
+}
+
 const commentListElem = document.getElementById("comment-list");
 
 const getList = async () => {
@@ -8,11 +23,37 @@ const getList = async () => {
 
   list.forEach((item) => {
     const tempLiElem = document.createElement("li");
-    tempLiElem.innerHTML = `${item.content} - ${item.name}`;
+    tempLiElem.innerHTML = `${item.content} - ${item.name} - ${dateChange(
+      item.createdAt
+    )}`;
     const tempOlElem = document.createElement("ul");
     item.children.forEach((child) => {
       const tempLiElem2 = document.createElement("li");
-      tempLiElem2.innerHTML = `${child.content} - ${child.name}`;
+      tempLiElem2.innerHTML = `${child.content} - ${child.name} - ${dateChange(
+        child.createdAt
+      )}`;
+      ///////////////
+      const tempOlElem2 = document.createElement("ul");
+      child.children.forEach((child2) => {
+        const tempLiElem3 = document.createElement("li");
+        tempLiElem3.innerHTML = `${child2.content} - ${
+          child2.name
+        } - ${dateChange(child2.createdAt)}`;
+        /////////////////////
+        const tempOlElem3 = document.createElement("ul");
+        child2.children.forEach((child3) => {
+          const tempLiElem4 = document.createElement("li");
+          tempLiElem4.innerHTML = `${child3.content} - ${
+            child3.name
+          } - ${dateChange(child3.createdAt)}`;
+          tempOlElem3.append(tempLiElem4);
+        });
+        tempLiElem3.append(tempOlElem3);
+        ////////////////////
+        tempOlElem2.append(tempLiElem3);
+      });
+      tempLiElem2.append(tempOlElem2);
+      ////////////////
       tempOlElem.append(tempLiElem2);
     });
     tempLiElem.append(tempOlElem);
@@ -21,3 +62,15 @@ const getList = async () => {
   });
 };
 getList();
+
+function reply(a) {
+  const tempOl = document.createElement("ul");
+  a.children.forEach((b) => {
+    const tempLi = document.createElement("li");
+    tempLi.innerHTML = `${b.content} - ${b.name}`;
+    if (b.children != null) {
+      reply(b);
+    }
+    tempOl.append(tempLi);
+  });
+}
